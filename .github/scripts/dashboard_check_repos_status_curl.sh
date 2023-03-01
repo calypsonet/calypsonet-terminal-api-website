a7b99269eeaf0c78e48691204a50ad1500caa2c3
Andrei Cristea <andrei.cristea019@gmail.com>
2023-03-01 11:40:55 +0100

    ci: update dashboard scripts

--- .github/scripts/dashboard_check_repos_status_curl.sh
+++ .github/scripts/dashboard_check_repos_status_curl.sh
@@ -18,22 +18,33 @@ echo "Checking repository "$repository_name"..."
 # - "size": Unknown reason of size changing.
 # - "pushed_at": Cyclic commit when gh-pages branch is update (calypsonet-terminal-api-website).
 
-github_hash=`curl --request GET \
+github_json=`curl --request GET \
           --url https://api.github.com/repos/calypsonet/$repository_name \
           --header "authorization: Bearer $token" \
-          --header "content-type: application/json" | grep -v -e "size"$filter_website_repository | md5sum | cut -d ' ' -f 1`
+          --header "content-type: application/json" | grep -v -e "size"$filter_website_repository`
+github_hash=`echo $github_json | md5sum | cut -d ' ' -f 1`
 
-dashboard_hash=`curl --request GET \
+dashboard_json=`curl --request GET \
           --url https://terminal-api.calypsonet.org/dashboard/$repository_name"_.json" \
-          --header "content-type: application/json" | grep -v -e "size"$filter_website_repository | md5sum | cut -d ' ' -f 1`
-
-echo "github_hash="$github_hash
-echo "dashboard_hash="$dashboard_hash
+          --header "content-type: application/json" | grep -v -e "size"$filter_website_repository`
+dashboard_hash=`echo $dashboard_json | md5sum | cut -d ' ' -f 1`
 
 if [ "$github_hash" = "$dashboard_hash" ]; then
   echo "No update required"
   exit 1
 else
   echo "Update required"
+  echo "################################################################################"
+  echo "     Github JSON hash = "$github_hash
+  echo "  Dashboard JSON hash = "$dashboard_hash
+  echo "###################"
+  echo "##  Github JSON  ##"
+  echo "###################"
+  echo $github_json
+  echo "######################"
+  echo "##  Dashboard JSON  ##"
+  echo "######################"
+  echo $dashboard_json
+  echo "################################################################################"
   exit 0
 fi
